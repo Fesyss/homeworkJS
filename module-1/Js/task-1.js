aa5139ad7a22b29283a294b5f08762306c963609
Rostislav <rudenkorostislav0@gmail.com>
2021-01-22 01:58:05 +0200

    Add module -1
    Add module 1
    Add Css
    Add Html
    Add Js
    Make task 1 js

---
+++ module-1/Js/task-1.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const str = "Привіт";
+
+console.log(typeof str);
+
+const num = 123;
+
+console.log(typeof num);
+
+const flag = true;
+
+console.log(typeof flag);
+
+const txt = "true";
+
+console.log(typeof txt);
+
+
+
+
+const message = "Hello";
+console.log(typeof message);
+const number = 149;
+console.log(typeof number);
+const boolean = true;
+console.log(typeof boolean);
+const houseNumber = 5;
+console.log(typeof houseNumber);
+const name = "Rostik";
+console.log(typeof name);
+
+let сar = "BMW";
+console.log(typeof car);
+let money = 100;
+console.log(typeof money);
+let surname = "Rudenko";
+console.log(typeof surname);
+let crab = false;
+console.log(typeof crab);
+let lobster = "false";
+console.log(typeof lobster);
+
+
+
+
+
+
+
+
+
+
+
