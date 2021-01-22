ce165b8922cc7b3352975b7ad9e556bbeb53e817
Rostislav <rudenkorostislav0@gmail.com>
2021-01-23 01:11:51 +0200

    make homework

---
+++ module-1/Js/task-2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// let name = prompt("Дайте  ім’я");
+// console.log( name  , "імя" );
+// let surname = prompt("Дайте своє  призьвісько");
+// console.log( surname , "фамілія");
+
+const myName = "Ростик";
+const mySurname = "Руденко";
+const nameMyFriend = "Богдан";
+const surnameMyFriend = "Таванов"; 
+alert(`Мене звати ${myName}, моя фамілія ${mySurname}.У мене є друг, його звати ${nameMyFriend} ${surnameMyFriend}` );
+
+
+
