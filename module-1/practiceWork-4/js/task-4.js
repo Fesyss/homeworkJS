a2df012f911417e61fb95e6c0cade1395fdd498f
Rostislav <rudenkorostislav0@gmail.com>
2021-02-07 00:46:26 +0200

    add 4 homework

--- module-1/practiceWork-4/js/task-4.js
+++ module-1/practiceWork-4/js/task-4.js
@@ -3,5 +3,17 @@ console.log(' кінець третьої задачі ↑ ');
 console.log(' початок четвертої задачі ↓  '); 
 
 
+// 4. Виберіть будь-яке число від 1 до 5 включно через функцію random. За допомогою if else визначіть, що це за число.
 
-
+const random = Math.round(Math.random() * 4 + 1);
+if (random === 1){
+    console.log(" вам випало число 1")
+}else if(random === 2){
+    console.log(" вам випало число 2")
+}else if (random ===3){
+    console.log(" вам випало число 3")
+}else if (random === 4){
+    console.log(" вам випало число 4")
+}else if(random === 5){
+    console.log(" вам випало число 5")
+}
--- module-1/practiceWork-4/js/task-5.js
+++ module-1/practiceWork-4/js/task-5.js
@@ -2,3 +2,12 @@
 console.log(' кінець четвертої задачі ↑ ');  
 console.log(" початок п'ятої задачі ↓  ");
 
+// 5. Змінна lang може приймати 4 значення: 'ua', 'en', 'ru', 'fr'. За допомогою конструкції switch виведи в консоль назву місяця в залежності від обраної мови. Наприклад, якщо lang = 'fr', то в консоль виводимо "Janvier".
+
+ const lang = prompt('Введіть мову: en , fr , ua , ru');
+ switch(lang ){
+   case "en": console.log("January");break;
+   case "fr": console.log("Janvier");break;
+   case "ua": console.log("Січень");break;
+   case "ru": console.log("Январь");break;
+ }
